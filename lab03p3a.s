37e7774b322a3969f576893143e1bfd083904ed8
yagurlandy <andylopezmartine@u.boisestate.edu>
2026-01-29 16:02:32 -0700

    Add comments to main function in lab03p3a.s
    Added comments to clarify the purpose of instructions.

--- lab03p3a.s
+++ lab03p3a.s
@@ -6,9 +6,9 @@ _start:
     .func main
     push {r4,r5,r6,r7,lr}
 
-    ldr r4, =(1<<31)-1
-    mov r5, #4
-    adds r2, r4, r5
+    ldr r4, =(1<<31)-1 // Loads 32-bit constant value into register r4
+    mov r5, #4 // Moves content of r4 to r5
+    adds r2, r4, r5 // Adds values of r4, r5 and puts the results in r2 without any effect on the status register flag
 
     all_done:
 
