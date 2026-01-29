c7f24104361dbb86ab7f8ab28b882f7f112fcb69
yagurlandy <andylopezmartine@u.boisestate.edu>
2026-01-29 22:41:31 +0000

    Changed to include best practice formatting

--- lab03p3a.s
+++ lab03p3a.s
@@ -1,12 +1,19 @@
 .global _start
 _start:
+
     .func main
     push {r4,r5,r6,r7,lr}
+
     ldr r4, =(1<<31)-1
     mov r5, #4
     adds r2, r4, r5
+
     all_done:
+
     pop {r4,r5,r6,r7,lr}
+
     bx lr
+
     .endfunc
+    
     .end
