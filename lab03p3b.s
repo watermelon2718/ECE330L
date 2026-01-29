76eb96203bef38b9221ad38ea0b4e9ffb95d33e0
yagurlandy <andylopezmartine@u.boisestate.edu>
2026-01-29 22:40:27 +0000

    Updated template file to reflect provided code

---
+++ lab03p3b.s
@@ -0,0 +1,12 @@
+.global _start
+_start:
+    .func main
+    push {r4,r5,r6,r7,lr}
+    mov r4, #7
+    mov r5, #13
+    subs r6, r4, r5
+    all_done:
+    pop {r4,r5,r6,r7,lr}
+    bx lr
+    .endfunc
+    .end
