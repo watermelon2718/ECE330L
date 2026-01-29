10ba95d6717d0fddd2fabf391c40e1255d3efa8b
yagurlandy <andylopezmartine@u.boisestate.edu>
2026-01-29 16:03:44 -0700

    Added comments for clarity in assembly code

--- lab03p3b.s
+++ lab03p3b.s
@@ -6,9 +6,10 @@ _start:
     .func main
     push {r4,r5,r6,r7,lr}
 
-    mov r4, #7
-    mov r5, #13
-    subs r6, r4, r5
+    mov r4, #7 // Moves contents of R7 to R4
+    mov r5, #13 // Moves contents of R13 to R5
+    subs r6, r4, r5 // r6=r4-r5 without affecting status register flag
+
 
     all_done:
 
